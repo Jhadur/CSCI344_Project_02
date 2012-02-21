b822ddcb9259a60df2a554812e6eb2bf0ad2f209
Jason Trump <jtrump@brbj.com>
2012-02-21 11:01:57 -0500

    cleaned code & commented

--- scripts/src/script.js
+++ scripts/src/script.js
@@ -1,8 +1,3 @@
-/**
- * This is the entry point for our JavaScript program
- */
-
- 
 $(function () {
 	
 	
@@ -22,82 +17,65 @@ $(function () {
 	
 	
 	s.register(function(tweet) {
-	
-	
-	//alert("hello world!");
-	
-	//$("#tweets").append("<p>"+tweet.text+"</p>");
-	
-    //2. Add profile images (tweet.profile_image_url)
-	
-	/*var profile_image = "<img src='"+tweet.profile_image_url+"' />";
-	profile_image.hide();
-	$("#tweets").prepend("<p>"+profile_image+tweet.text+"</p>");
-	profile_image.slideDown();*/
-	//"<li style='display:none;color:blue;background-color:white;'>"
-	
+
 	s.stop();
-	
-	
-	
-	
+			
 	$(function() {
 		phrase = false
 		val = tweet.text
-		
-			phrase = new Phrase($('#phrase-module'), val);
-			$(window).scrollTop();
-			$('body, html').animate({scrollTop: $(this).offset()}, 500);
-		});
+		phrase = new Phrase($('#phrase-module'), val);
+		$(window).scrollTop();
+		$('body, html').animate({scrollTop: $(this).offset()}, 500);
+	});
 
 
 
 	
-function Phrase($elem, phrase) {
-    var self = this;
-    this.$phrase = $elem.find('.phrase');
-    this.$pattern = $elem.find('.pattern');
-    this.$legend = $elem.find('.legend');
-    this.$stats = $elem.find('.stats');
-    this.phrase = $.trim(phrase);
-    this.getData = function() {
-        this.phrase_length = this.phrase.length;
-        this.word_count = this.phrase.split(/\s+/).length;
-        this.letter_count = 0;
-        this.vowel_count = 0;
-        this.conso_count = 0;
-        this.letters = [];
-        var l, counts = [], alphabet = 'abcdefghijklmnopqrstuvwxyz', vowels = 'aeiou';
-        for (var i = 0, len = this.phrase.length; i < len; i++) {
-            l = this.phrase.charAt(i).toLowerCase();
-            if (alphabet.indexOf(l) > -1) {
-                this.letter_count++;
-                this._countLetter(this.phrase.charAt(i).toLowerCase());
-                if (vowels.indexOf(l) > -1) {
-                    this.vowel_count++;
-                } 
-                else {
-                    this.conso_count++;
-                }
-            }
-        }
-        this.letters.sort(function(a, b) {
-            if ((a.count * 1) < (b.count * 1)) {
-                return 1;
-            } 
-            else if ((a.count * 1) > (b.count * 1)) {
-                return -1;
-            } 
-            else if (self.phrase.toLowerCase().indexOf(a.letter) < self.phrase.toLowerCase().indexOf(b.letter)) {
-                return -1;
-            } 
-            else if (self.phrase.toLowerCase().indexOf(a.letter) > self.phrase.toLowerCase().indexOf(b.letter)) {
-                return 1;
-            }
-            return 0;
-        });
-        this._setPercentages();
-    };
+	function Phrase($elem, phrase) {
+		var self = this;
+		this.$phrase = $elem.find('.phrase');
+		this.$pattern = $elem.find('.pattern');
+		this.$legend = $elem.find('.legend');
+		this.$stats = $elem.find('.stats');
+		this.phrase = $.trim(phrase);
+		this.getData = function() {
+			this.phrase_length = this.phrase.length;
+			this.word_count = this.phrase.split(/\s+/).length;
+			this.letter_count = 0;
+			this.vowel_count = 0;
+			this.conso_count = 0;
+			this.letters = [];
+			var l, counts = [], alphabet = 'abcdefghijklmnopqrstuvwxyz', vowels = 'aeiou';
+			for (var i = 0, len = this.phrase.length; i < len; i++) {
+				l = this.phrase.charAt(i).toLowerCase();
+				if (alphabet.indexOf(l) > -1) {
+					this.letter_count++;
+					this._countLetter(this.phrase.charAt(i).toLowerCase());
+					if (vowels.indexOf(l) > -1) {
+						this.vowel_count++;
+					} 
+					else {
+						this.conso_count++;
+					}
+				}
+			}
+			this.letters.sort(function(a, b) {
+				if ((a.count * 1) < (b.count * 1)) {
+					return 1;
+				} 
+				else if ((a.count * 1) > (b.count * 1)) {
+					return -1;
+				} 
+				else if (self.phrase.toLowerCase().indexOf(a.letter) < self.phrase.toLowerCase().indexOf(b.letter)) {
+					return -1;
+				} 
+				else if (self.phrase.toLowerCase().indexOf(a.letter) > self.phrase.toLowerCase().indexOf(b.letter)) {
+					return 1;
+				}
+				return 0;
+			});
+			this._setPercentages();
+		};
     this._countLetter = function(letter) {
         for (var i = 0, len = this.letters.length; i < len; i++) {
             if (this.letters[i].letter === letter) {
@@ -159,40 +137,29 @@ function Phrase($elem, phrase) {
     this.makeLegend();
     this.makeStats();
     $('#phrase-module').css({position: 'relative',left: 0,display: 'none'}).fadeIn(300);
-}
-function Circle(phrase, radius, letter) {
-    this.phrase = phrase;
-    this.id = phrase._circle_id;
-    phrase._circle_id++;
-    this.radius = radius;
-    var $temp = $('<div class="' + letter + '" style="display:none;"></div>').appendTo('body');
-    this.color = $temp.css('background-color');
-    $temp.remove();
-    this.draw = function() {
-        var ctx = this.phrase.ctx, xy = this.phrase.$pattern.width() / 2;
-        ctx.fillStyle = this.color;
-        ctx.beginPath();
-        ctx.arc(xy, xy, this.radius, 0, 360, false);
-        ctx.closePath();
-        ctx.fill();
-    };
-}
-		
-		
-		
-		
-		
-	});
+	
+	}
+	function Circle(phrase, radius, letter) {
+		this.phrase = phrase;
+		this.id = phrase._circle_id;
+		phrase._circle_id++;
+		this.radius = radius;
+		var $temp = $('<div class="' + letter + '" style="display:none;"></div>').appendTo('body');
+		this.color = $temp.css('background-color');
+		$temp.remove();
+		this.draw = function() {
+			var ctx = this.phrase.ctx, xy = this.phrase.$pattern.width() / 2;
+			ctx.fillStyle = this.color;
+			ctx.beginPath();
+			ctx.arc(xy, xy, this.radius, 0, 360, false);
+			ctx.closePath();
+			ctx.fill();
+		};
+	}
+	}); //ENDS s.register(function(tweet)
 	
 	s.start();
 	
 	});
-    //3. Make the tweets occur so the most recent are at the top
-    //4. Make the tweets slide down
-    //5. Alternate the colors or the background of the tweets
-    //6. Show a maximum of 10 tweets at a time (remove old tweets from the dom)
-	
-	
-
 });
 
